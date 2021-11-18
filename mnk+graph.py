98fd808754ef1696eb64e1e12f1228a507bee666
BobaBiven <91140696+BobaBiven@users.noreply.github.com>
2021-11-19 02:40:34 +0300

    Update mnk+graph.py

--- mnk+graph.py
+++ mnk+graph.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy
 
-a = numpy.loadtxt('C:\\Users\\Askar\\PycharmProjects\\pythonProject\\.txt', dtype=float)
-b = numpy.loadtxt('C:\\Users\\Askar\\PycharmProjects\\pythonProject\\.txt', dtype=float)
+a = numpy.loadtxt('C:\\Users\\Askar\\PycharmProjects\\pythonProject\\i.txt', dtype=float)
+b = numpy.loadtxt('C:\\Users\\Askar\\PycharmProjects\\pythonProject\\r.txt', dtype=float)
 
 x = 0
 y = 0
@@ -16,21 +16,25 @@ for i in range (len(a)):
     x2 += a[i] ** 2
 n = len(a)
 k = (n * xy - x * y) / (n * x2 - x ** 2)
-b = (y - k * x) / n
+d = (y - k * x) / n
 
+a1 = 0
+a2 = max(a)
+b1 = k * a1 + d
+b2 = k * a2 + d
+
+print(k, d, sep='\n')
 
 ax = plt.subplot()
-ax.plot()
+ax.plot((a1, a2), (b1, b2))
+
 
 plt.grid(True)
 plt.title("")
 plt.xlabel("")
 plt.ylabel("")
-plt.xlim(0, max(b) * 1.1)
-plt.ylim(0, max(a) * 1.1)
+plt.xlim(0, max(a) * 1.1)
+plt.ylim(0, max(b) * 1.1)
 plt.scatter(a, b)
 plt.savefig('C:\\Users\\Askar\\PycharmProjects\\pythonProject\\graph.png')
 plt.show()
-
-
-
