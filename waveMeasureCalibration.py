69e7325ba892f3ae850803722a978b065a70e7bd
BobaBiven <91140696+BobaBiven@users.noreply.github.com>
2021-12-06 22:44:15 +0300

    Create waveMeasureCalibration.py

---
+++ waveMeasureCalibration.py
@@ -0,0 +1,21 @@
+import waveFunctions as b
+import spidev
+import time
+import RPi.GPIO as GPIO
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+spi = spidev.SpiDev()
+
+b.initSpiAdc()
+measure_x = []
+t0 = time.time()
+t = t0
+while t - t0 < 10:
+    t = time.time()
+    measure_x.append(b.getAdc())
+
+b.deinitSpiAdc()
+
+b.save(measure_x, t0, t)
